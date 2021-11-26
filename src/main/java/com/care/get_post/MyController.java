ac75ad2737f166c40a2c2ec0860dfd07bfc4ca12
brown <ohbass95@gmail.com>
2021-11-26 21:42:36 +0900

    spring05

--- src/main/java/com/care/get_post/MyController.java
+++ src/main/java/com/care/get_post/MyController.java
@@ -1,9 +1,14 @@
 package com.care.get_post;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MyController {
@@ -17,15 +22,33 @@ public class MyController {
 	}
 	
 	@GetMapping("my/result")
-	public String result_get() {
+	public String result_get(HttpServletRequest request, Model model) {
+		String name = request.getParameter("name");
+		String age = request.getParameter("age");
+		
 		System.out.println("get방식으로 들어왔습니다");
+		System.out.println("name : "+name);
+		System.out.println("age : "+age);
+		
+		model.addAttribute("method", request.getMethod());
+		model.addAttribute("name", name);
+		model.addAttribute("age", age);
+		
 		return "/get_post/result";
 	}
 //	@PostMapping("my/result")
 	
 	@RequestMapping(value="my/result", method = RequestMethod.POST)//get방식
-	public String result_post() {
+	public String result_post(HttpServletRequest request, Model model,
+									@RequestParam("name") String name,//값을 가져와서 저장한다
+									@RequestParam("age") String age) {
 		System.out.println("post방식으로 들어왔습니다");
+		
+		model.addAttribute("method", request.getMethod());
+		model.addAttribute("name", name);
+		model.addAttribute("age", age);
+		
+		
 		return "/get_post/result";
 	}
 }
--- src/main/webapp/WEB-INF/views/get_post/result.jsp
+++ src/main/webapp/WEB-INF/views/get_post/result.jsp
@@ -6,7 +6,9 @@
 <meta charset="EUC-KR">
 <title>Insert title here</title>
 </head>
-<body>
-
+<body>result.jsp<br>
+	<h1>${method }</h1>
+	<h3>name : ${name }</h3>
+	<h3>age : ${age }</h3>
 </body>
 </html>
